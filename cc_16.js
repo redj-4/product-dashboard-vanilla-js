6071901aa5ea338cacace3df9fe10f1c89fed260
Divya Joshi <divyaj.redj@gmail.com>
2025-03-30 21:19:49 -0400

    Task 3 -     Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -17,3 +17,18 @@ function fetchProductsThen() { //creating function to fetch products
     });
 }
 
+//Task 3: : Fetch Products with async/await
+async function fetchProductsAsync() {
+    try {
+      const response = await fetch("https://www.course-api.com/javascript-store-products");
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const products = await response.json();
+      // Call helper function to display products on the page
+      displayProducts(products);
+    } catch (error) {
+      // If an error occurs, handle it with the provided helper
+      handleError(error);
+    }
+  }
